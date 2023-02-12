7630c260201157c551bbbc4a5d0ea5f0f2cca25c
shio <85730998+dino3616@users.noreply.github.com>
2023-02-12 14:27:31 +0000

    feat: ✨ (eslint) eslint-config-manaslu-nextjsを作成

---
+++ packages/eslint/eslint-config-manaslu-nextjs/index.js
@@ -0,0 +1,90 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  $schema: "https://json.schemastore.org/eslintrc",
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-typescript",
+    "eslint:recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "next",
+    "plugin:react/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  plugins: ["@typescript-eslint", "react", "jsx-a11y", "import", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/no-default-export": "error",
+    "import/no-extraneous-dependencies": ["error", { packageDir: ["./"] }],
+    "import/order": [
+      "error",
+      {
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "internal",
+            position: "before",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "react/jsx-props-no-spreading": "off",
+    "react/no-danger": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+  },
+  overrides: [
+    {
+      files: ["./*"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "import/no-default-export": "off",
+        "import/prefer-default-export": "off",
+      },
+    },
+    {
+      files: ["./src/pages/**"],
+      rules: {
+        "import/no-default-export": "off",
+        "import/prefer-default-export": "error",
+      },
+    },
+    {
+      files: ["./src/**/*.story.*"],
+      rules: {
+        "import/no-default-export": "off",
+        "import/no-extraneous-dependencies": "off",
+        "import/prefer-default-export": "off",
+      },
+    },
+  ],
+};
