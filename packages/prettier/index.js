be4a8e1eae2f25c54b275b03ac12708cc16aba34
shio <85730998+dino3616@users.noreply.github.com>
2023-02-12 14:28:27 +0000

    feat: ✨ (prettier) prettierを管理するpakcageを作成

---
+++ packages/prettier/index.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Options} */
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+};
